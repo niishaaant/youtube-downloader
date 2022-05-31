35b48fb14dab133d1fc256981acc2cd0730194e0
niishaaant <nishant98680042@gmail.com>
2022-06-01 03:13:09 +0530

    init

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import Navbar from './Navbar';
+import Main from './Main';
+
+export const App = () => (
+	<ChakraProvider theme={theme}>
+		<Navbar />
+		<Main />
+	</ChakraProvider>
+);
